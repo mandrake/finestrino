875bafb4290074b40db8dae22dce86538dc0754b
Antonio Malara <antonio.malara@booking.com>
2015-12-21 17:52:16 +0100

    adding a renderer for the ele files

--- corridori.py
+++ corridori.py
@@ -20,9 +20,28 @@ class ResourcesPaths(object):
     def arcade_palette(self):         return self.gamedir("AR1/STA/ARCADE.PAL")
     def background_tileset(self, nr): return self.gamedir("AR1/STA/BUFFER%X.MAT" % nr)
     def room_roe(self):               return self.gamedir("AR1/MAP/ROOM.ROE")
+    def k_ele(self):                  return self.gamedir("AR1/IMG/K.ELE")
+    def tr_ele(self):                 return self.gamedir("AR1/IMG/TR.ELE")
+    def ucc_ele(self):                return self.gamedir("AR1/IMG/UCCI0.ELE")
+
+def load_file(path):
+    return [ord(i) for i in open(path, 'rb').read()]
+
+def skip(l, n):
+    for _ in xrange(n): next(l)
+
+def oneoff(l):
+    next(l)
+    return next(l)
+
+def word(l):
+    return next(l) + (next(l) << 8)
+
+def dword(l):
+    return word(l) + (word(l) << 16)
 
 def load_palette(path):
-    data        = [ord(i) for i in open(path, 'r').read()]
+    data        = load_file(path)
     first_color = data[0]
     count       = data[1]
 
@@ -39,7 +58,7 @@ def load_palette(path):
 def get_background_tiles(path, pal):
     TILE_BYTE_SIZE = TILE_WIDTH * TILE_HEIGTH
 
-    data  = [ord(i) for i in open(path, 'r').read()]
+    data  = load_file(path)
     tiles = []
 
     for tile_i in xrange((320 / TILE_WIDTH) * (200 / TILE_HEIGTH)):
@@ -54,20 +73,9 @@ def get_background_tiles(path, pal):
     return tiles
 
 def load_room_description(path):
-    size = 0x4f4
-    data = [ord(i) for i in open(path, 'rb').read()]
-    base = 0x24
-
-    def skip(l, n):
-        for _ in xrange(n): next(l)
-
-    def oneoff(l):
-        next(l)
-        return next(l)
-
-    def word(l):
-        return next(l) + (next(l) << 8)
-
+    data  = load_file(path)
+    size  = 0x4f4
+    base  = 0x24
     rooms = []
 
     for i in xrange(len(data) / size):
@@ -109,6 +117,87 @@ def blit_room(room, tilesets, surface):
 
             surface.blit(bitmap, (x * TILE_WIDTH, y * TILE_HEIGTH))
 
+def load_ele_file(path):
+    data        = load_file(path)
+    ele_file    = iter(data)
+    count       = word(ele_file)
+    relocs      = [dword(ele_file) for _ in xrange(count)]
+    images_data = [data[i + 2:] for i in relocs]
+    images      = []
+
+    for i in images_data:
+        ele_item = iter(i)
+        width    = word(ele_item)
+        heigth   = word(ele_item)
+
+        skip(ele_item, 1)
+
+        lines = []
+        for _ in xrange(heigth):
+            line = []
+            lines.append(line)
+
+            while True:
+                thing = next(ele_item)
+                line.append(thing)
+                if thing == 0xff:
+                    break
+
+        lines = reduce(lambda a, b: a + b, lines) + [0xff, 0xff]
+
+        images.append((width, heigth, lines))
+
+
+    return images
+
+def render_ele_item(item, palette, col=-63):
+    width, heigth, lines = item
+
+    the_ele        = iter(lines)
+    consecutive_ff = 0
+    surface        = pygame.Surface((width, heigth))
+    cur_x, cur_y   = 0, 0
+
+    while True:
+        skip = next(the_ele)
+
+        if skip != 0xff:
+            consecutive_ff = 0
+            cur_x += skip
+
+            count = next(the_ele)
+
+            if count != 0xff:
+                consecutive_ff = 0
+
+                for _ in xrange(count / 2):
+                    colors = next(the_ele)
+                    color1 = (colors & 0x0f)      + col
+                    color2 = (colors & 0xf0 >> 4) + col
+
+                    surface.set_at((cur_x, cur_y), palette[color1])
+                    cur_x += 1
+                    surface.set_at((cur_x, cur_y), palette[color2])
+                    cur_x += 1
+
+                if count & 1:
+                    color = next(the_ele)
+                    color1 = (color & 0x0f) + col
+                    surface.set_at((cur_x, cur_y), palette[color1])
+                    cur_x += 1
+            else:
+                consecutive_ff += 1
+                if consecutive_ff == 3:
+                    return surface
+        else:
+            cur_x = 0
+            cur_y += 1
+
+            consecutive_ff += 1
+            if consecutive_ff == 3:
+                return surface
+
+
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_mode(WINDOW_SIZE)
@@ -121,6 +210,7 @@ if __name__ == '__main__':
     palette    = load_palette(resources.arcade_palette())
     tilesets   = {i: get_background_tiles(resources.background_tileset(i), palette) for i in (1, 2, 3, 4, 5, 6, 10, 11)}
     rooms      = load_room_description(resources.room_roe())
+    kele       = [render_ele_item(i, palette) for i in load_ele_file(resources.k_ele())]
 
     current_room = 0
 
@@ -143,6 +233,8 @@ if __name__ == '__main__':
         current_room = min(current_room, len(rooms) - 1)
 
         blit_room(rooms[current_room], tilesets, screen)
+
+        screen.blit(kele[0], (10, 10))
 
         pygame.transform.scale(screen, WINDOW_SIZE, pygame.display.get_surface())
         pygame.display.flip()
